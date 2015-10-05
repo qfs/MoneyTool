0a336be49b3966ee1ddb879e79ee27d903fb647e
Pascal Bihler <pascal@qfs.de>
2015-10-05 15:47:18 +0200

    Added Basic functionality

---
+++ test/com/thirdparty/MoneyToolTest.java
@@ -0,0 +1,31 @@
+package com.thirdparty;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MoneyToolTest {
+
+	MoneyTool tool;
+
+	@Before
+	public void setUpSystemUnderTest() {
+		tool = new MoneyTool();
+	}
+
+	@Test
+	public void testFormat() throws Exception {
+		final String result = tool.format(2312);
+
+		assertEquals("TLR 23,12",result);
+	}
+
+	@Test
+	public void testFormatNull() throws Exception {
+		final String result = tool.format(0);
+
+		assertEquals("TLR 0,00",result);
+	}
+
+}
